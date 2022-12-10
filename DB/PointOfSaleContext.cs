349dfda0fab7791471911b1231a7848f16103d52
Tadas Verbyla <tadas.verbyla@gmail.com>
2022-12-10 20:54:21 +0200

    Created DB folder and PointOfSaleContext class for DbContext.

---
+++ DB/PointOfSaleContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Point_of_Sale_Lab3.Data_Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point_of_Sale_Lab3.DB
+{
+    internal class PointOfSaleContext : DbContext
+    {
+        public PointOfSaleContext(): base()
+        {
+
+        }
+
+        public DbSet<Checkout> checkouts { get; set; }
+        public DbSet<Complaint> complaints { get; set; }
+        public DbSet<Employee> employees { get; set; }
+        public DbSet<ModelAndView> modelAndViews { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<Permission> permissions { get; set; }
+        public DbSet<Review> reviews { get; set; }
+        public DbSet<Shift> shifts { get; set; }
+        public DbSet<Support> support { get; set; }
+        public DbSet<Timestamp> timestamps { get; set; }
+        public DbSet<User> users { get; set; }
+        public DbSet<View> views { get; set; }
+
+    }
+}
