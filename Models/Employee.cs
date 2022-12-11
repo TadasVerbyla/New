7d2b106997205818576bfd82b61f66fc7cb6d049
Tadas Verbyla <tadas.verbyla@gmail.com>
2022-12-11 18:29:00 +0200

    Changed Class Data folder name to Models

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Point_of_Sale_Lab3.Data_Classes
+namespace Point_of_Sale_Lab3.Models
 {
     public class Employee
     {
