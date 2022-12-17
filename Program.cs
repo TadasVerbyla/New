ed904200580e481aac82ffcf7daf302e53abb254
Tadas Verbyla <tadas.verbyla@gmail.com>
2022-12-17 13:33:37 +0200

    Refactored DB table names and added example code to Program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Point_of_Sale_Lab3.DB;
+using Point_of_Sale_Lab3.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,12 @@ namespace Point_of_Sale_Lab3
     {
         static void Main(string[] args)
         {
-
+            using (var ctx = new PointOfSaleContext())
+            {
+                var complaint = new Complaint() { id = 1, orderId = 2, text = "complaint" };
+                ctx.Complaints.Add(complaint);
+                ctx.SaveChanges();
+            }
         }
     }
 }
