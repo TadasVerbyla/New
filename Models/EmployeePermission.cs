a64e899e624a708e9db3056f55b93eba87c360f3
Rytis Mikalauskas <mikrytis@gmail.com>
2022-12-28 23:19:44 +0200

    db fix

--- Models/EmployeePermission.cs
+++ Models/EmployeePermission.cs
@@ -1,9 +1,12 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Point_of_Sale_Lab3.Models
 {
     public class EmployeePermission
     {
+        [Key]
         public Guid employeeId { get; set; }
-        public Guid permissionId { get; set; }
+        public Permission permission { get; set; }
     }
 }
--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -1,7 +1,10 @@
-﻿namespace Point_of_Sale_Lab3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Point_of_Sale_Lab3.Models
 {
     public class OrderItem
     {
+        [Key]
         public Guid orderId { get; set; }
         public Guid itemId { get; set; }
         public int quantity { get; set; }
